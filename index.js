6347113b6ae021be5f80cbeb4ff1f7f88c2dc495
Bill Nguyen <billfl93@gmail.com>
2022-08-26 23:17:58 -0400

    commit tree d3bac57a109306f9a56a763526a051af585c12a3

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word);
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  that means if the word is the same as the word in reverse, I should return true.
+  
+  reverse the input string
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
 /*
